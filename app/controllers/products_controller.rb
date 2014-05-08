46b44305a204d59c883478cd4d6519361ca1890a
Josh Sullivan <jsullivan@sportslabs.com>
2014-05-07 22:23:23 -0600

    I don't remember

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,10 +17,10 @@ class ProductsController < ApplicationController
 			format.json { render :json => @straps }
 		end
 	end
-	
+
 	#Status enums
 		STATUS = { :available => 0, :hold => 1, :sold => 2, :unavailable => 3 }
-	
+
 	#Category enums
 		CATEGORY = { :vintage => 0, :new => 1, :merch => 3 }
 end
